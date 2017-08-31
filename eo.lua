7c7a5c20d6e66f659885d4e7b345da39dfe5a866
Stas Mikhel <mikhel.sk@gmail.com>
2017-08-31 23:11:19 +0300

    Add Esperanto text converter

---
+++ eo.lua
@@ -0,0 +1,28 @@
+------------------------------------------------------------------
+-- Convert text to Esperanto version by replacing               --
+-- combinations such like 'cx' or 'Cx' to ĉ and Ĉ respectively. --
+------------------------------------------------------------------
+
+-- check arguments
+if not arg[1] then
+   print(string.format('Usage: lua %s file_to_convert', arg[0]))
+   os.exit()
+end
+-- rules
+convert = {C='Ĉ',G='Ĝ',H='Ĥ',J='Ĵ',S='Ŝ',U='Ǔ',
+           c='ĉ',g='ĝ',h='ĥ',j='ĵ',s='ŝ',u='ǔ'}
+setmetatable(convert, {__index=function(a,b) return b..'x' end})
+-- prepare new file
+tmpname = arg[1] .. '(1)'
+res = io.open(tmpname, 'w')
+-- convert and write
+for line in io.lines(arg[1]) do
+   res:write(string.gsub(line, '(.)x', convert), '\n')
+end
+-- save
+res:flush(); res:close()
+-- change
+os.remove(arg[1])
+os.rename(tmpname, arg[1])
+print("Done!")
+
